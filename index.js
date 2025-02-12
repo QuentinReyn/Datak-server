7bded9e52669fa0fdcfaaca341801b9671ac8ce7
QuentinReyn <quentin.reynaud99@gmail.com>
2025-02-12 19:18:41 +0100

    update origin cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://datak.vercel.app",
+    origin: "https://datak.onrender.com",
     methods: ["GET", "POST"],
     transports: ['websocket', 'polling'],
     credentials: true
